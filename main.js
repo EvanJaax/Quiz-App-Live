9d9ad1e66a43078ae7d1e71bd114005913b5fb78
EvanJaax <60020749+EvanJaax@users.noreply.github.com>
2020-03-06 22:33:23 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
 // let tryCount = 1
+$(function() {
+  $('body').scrollTop(0);
+});
 let vh = window.innerHeight * 0.01;
 document.documentElement.style.setProperty('--vh', `${vh}px`);
 window.addEventListener('resize', () => {
